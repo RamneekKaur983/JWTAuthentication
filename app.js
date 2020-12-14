a788a9d79b6f298f065619c4e8049e83ea1e2c86
unknown <ramneek983@gmail.com>
2020-12-15 02:47:06 +0530

    JWT

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+mongoose.connect('mongodb+srv://Ramneek:iamunique@cluster0.cyzml.mongodb.net/JWT', {useNewUrlParser: true , useUnifiedTopology: true});
+app.use(authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
